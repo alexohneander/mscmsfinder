83650b8ecc71890fbda4002982bddd54e031d977
Alex Wellnitz <a.wellnitz@apozin.de>
2023-01-24 21:45:18 +0100

    Get HTTP Response.

--- client/http.go
+++ client/http.go
@@ -1,13 +1,24 @@
 package client
 
-import "mscmsfinder/helper"
+import (
+	"mscmsfinder/helper"
+	"net/http"
+)
 
 func RequestURL(url string) string {
 	message := ""
 
 	if !helper.IsValidURL(url) {
 		message = "Url is not valid"
-		return message
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		message = "Cannot request URL"
+	}
+
+	if resp.StatusCode == 200 {
+		message = "Request was successfull"
 	}
 
 	return message
