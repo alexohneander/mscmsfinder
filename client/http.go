7ab49d5f7ca5ff56d95449856b9f2eef102adfcb
Alex Wellnitz <a.wellnitz@apozin.de>
2023-01-24 23:20:38 +0100

    Building api logic.

--- client/http.go
+++ client/http.go
@@ -5,14 +5,15 @@ import (
 	"net/http"
 )
 
-func RequestURL(url string) string {
+func TestEndpoint(url string, path string) string {
 	message := ""
+	fullUrl := url + path
 
-	if !helper.IsValidURL(url) {
+	if !helper.IsValidURL(fullUrl) {
 		message = "Url is not valid"
 	}
 
-	resp, err := http.Get(url)
+	resp, err := http.Get(fullUrl)
 	if err != nil {
 		message = "Cannot request URL"
 	}
--- handler/cms.go
+++ handler/cms.go
@@ -1,7 +1,10 @@
 package handler
 
 import (
+	"fmt"
 	"mscmsfinder/client"
+	"mscmsfinder/database"
+	"mscmsfinder/model"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -12,7 +15,22 @@ func FindCMS(c *fiber.Ctx) error {
 	message := "requesting"
 
 	url := c.Query("url")
-	message = client.RequestURL(url)
+	message = TestEndpoints(url)
 
 	return c.JSON(fiber.Map{"status": "success", "message": message, "data": nil})
+}
+
+func TestEndpoints(url string) string {
+	message := "testing Endpoints"
+
+	db := database.DB
+	var systems []model.CMS
+	db.Find(&systems)
+
+	for _, cms := range systems {
+		message = client.TestEndpoint(url, cms.UrlPath)
+		fmt.Println(cms.Title)
+	}
+
+	return message
 }
